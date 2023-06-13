d06c0c6136ecbe02f680582171a6b503bda2d99c
Priyadarshini Muthuraman <priyadarshini.muthuraman@brother.co.uk>
2023-06-13 20:23:04 +0530

    Integrated delete and comple action of tasks

--- todo-app/src/components/listtodo/listtodo.js
+++ todo-app/src/components/listtodo/listtodo.js
@@ -2,17 +2,25 @@ import React from "react";
 import './listtodo.css';
 
 const Listtodo = (props) => {
-    const { list } = props;
+    const { list, deleteItem, completeItem, type } = props;
     return (
         <>
+            { list.length > 0 && type === 'completedTasks' && <h2>Completed Tasks</h2>}
             { list.length > 0 &&
                 <ul className="todo-list">
                     { list.map((todo, index) => (
                         <div className="row">
-                            <li key = {index} className="col"> 
+                            <li key = {index} className={`col ${(type === 'completedTasks' ? 'completed-task-lists' : 'new-tasks')}`}> 
                                 {todo} 
                             </li>
-                            <button className="col delete-button"> Delete </button>
+                             { 
+                                type === "newTasks" && <div className="complete-btn-wrapper col">
+                                    <button className="completed-button btn btn-outline-info" onClick={() => { completeItem(todo) }}> Mark as complete </button>
+                                </div> 
+                            }
+                            <div className="delete-btn-wrapper col">
+                                <button className="delete-button btn btn-danger" onClick={() => { deleteItem(todo, type) }}> Delete </button>
+                            </div>
                         </div>
                     ))}
                 </ul>
--- todo-app/src/components/todo/todo.js
+++ todo-app/src/components/todo/todo.js
@@ -5,35 +5,58 @@ import Listtodo from "../listtodo/listtodo";
 const ToDo = () => {
     const [todoItem, setTodoItem] = useState('');
     const [todoList, setTodoList] = useState([]);
+    const [completedTodoList, setCompletedTodoList] = useState([]);
 
     const addTodo = () => {
-        console.log(123);
         if(todoItem !== ''){
-            console.log(89898);
             setTodoList([...todoList, todoItem]);
             setTodoItem('');
         }
     }
-    console.log(todoList);
-    console.log(todoItem);
+    const deleteTodo = (deleteitem, type) => {
+        if(type === 'completedTasks'){
+            const updatedTodo = completedTodoList.filter((listItem) => {
+                return listItem !== deleteitem
+            });
+            setCompletedTodoList(updatedTodo);
+        } else {
+            const updatedTodo = todoList.filter((listItem) => {
+                return listItem !== deleteitem
+            });
+            setTodoList(updatedTodo);
+        }
+    }
+    const completeItem = (item) => {
+        deleteTodo(item);
+        setCompletedTodoList([...completedTodoList, item]);
+    }
     return (
         <div>
             <h1>React Todo App</h1>
-            <div className="input-wrapper">
-                <input 
-                    type="text" 
-                    name="todo" 
-                    placeholder="Create a new todo"
-                    value={todoItem}
-                    onChange={(e) => {
-                        setTodoItem(e.target.value);
-                    }}
-
-                />
-                <button className="add-button" onClick={addTodo}>Add</button>
+                <div className="input-wrapper">
+                <form className="form-inline mb-2">
+                    <div class="form-group">
+                        <input 
+                            type="text" 
+                            name="todo" 
+                            placeholder="Create a new todo"
+                            value={todoItem}
+                            onChange={(e) => {
+                                setTodoItem(e.target.value);
+                            }}
+                            className="form-control"
+                        />
+                    </div>
+                </form>
+                <div className="add-btn-wrapper">
+                    <button className="add-button btn btn-primary mb-2" onClick={addTodo}>Add</button>
+                </div>
             </div>
             <div className="todoList-wrapper">
-                <Listtodo list = {todoList}/>
+                <Listtodo list = {todoList} deleteItem={deleteTodo} completeItem={completeItem} type='newTasks'/>
+            </div>
+            <div className="todoList-wrapper">
+                <Listtodo list = {completedTodoList} deleteItem={deleteTodo} type='completedTasks'/>
             </div>
         </div>
     )
